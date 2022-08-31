bc6346a7cb15e31f643a50a854469e94282e1889
EricPuig10 <puigvendrelloetona@gmail.com>
2022-08-31 13:56:17 +0200

    map done

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,21 +1,32 @@
-import React, { useEffect, useState } from 'react'
-import { candidatsService } from '../../services/candidatsService'
-
+import React, { useEffect, useState } from "react";
+import { candidatsService } from "../../services/candidatsService";
 
 function List() {
-    const [candidats, setCandidats] = useState([])
-    const getAllCandidats = () => {
-        candidatsService.getAllCandidats().then((res)=>{
-            setCandidats(res);
-        });
-    }
-    useEffect(()=>{
-        getAllCandidats();
-    },[]);
-    console.log(candidats)
+  const [candidats, setCandidats] = useState([]);
+  const getAllCandidats = () => {
+    candidatsService.getAllCandidats().then((res) => {
+      setCandidats(res);
+    });
+  };
+  useEffect(() => {
+    getAllCandidats();
+  }, []);
+  console.log(candidats);
   return (
-    <div>List</div>
-  )
+    <div>
+      <div>List</div>
+      {candidats.map((candidat, key) => (
+        <div key={candidat.id}>
+          <p>{candidat.name}</p>
+          <p>{candidat.lastname}</p>
+          <p>{candidat.secondlastname}</p>
+          <p>{candidat.email}</p>
+          <p>{candidat.phone}</p>
+          <p>{candidat.age}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default List
+export default List;
