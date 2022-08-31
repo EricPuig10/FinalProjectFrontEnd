0e17f081d6045775227a5e6bcd30c52216a72e77
EricPuig10 <puigvendrelloetona@gmail.com>
2022-08-31 14:19:34 +0200

    table candidates ok

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -15,7 +15,7 @@ function List() {
   return (
     <div>
       <div>List</div>
-      {candidats.map((candidat, key) => (
+      {/* {candidats.map((candidat, key) => (
         <div key={candidat.id}>
           <p>{candidat.name}</p>
           <p>{candidat.lastname}</p>
@@ -24,7 +24,70 @@ function List() {
           <p>{candidat.phone}</p>
           <p>{candidat.age}</p>
         </div>
-      ))}
+      ))} */}
+      <table>
+        <tr>
+          <th>Name</th>
+
+          <th>Lastname</th>
+
+          <th>Second Name</th>
+
+          <th>Email</th>
+
+          <th>Phone</th>
+
+          <th>Age</th>
+
+          <th>Gender</th>
+
+          <th>Nationality</th>
+
+          <th>Laboral situation</th>
+
+          <th>Solo Learn Progress</th>
+
+          <th>Code Academy Progress</th>
+
+          <th>Assisted Informative Session</th>
+
+          <th>Bootcamp</th>
+
+          <th>Process State</th>
+        </tr>
+
+        {candidats.map((candidat, key) => (
+          <tr key={candidat.id}>
+            <td>{candidat.name}</td>
+
+            <td>{candidat.lastname}</td>
+
+            <td>{candidat.secondlastname}</td>
+
+            <td>{candidat.email}</td>
+
+            <td>{candidat.phone}</td>
+
+            <td>{candidat.age}</td>
+
+            <td>{candidat.gender}</td>
+
+            <td>{candidat.nationality}</td>
+
+            <td>{candidat.laboralsituation}</td>
+
+            <td>{candidat.sololearnprogress}</td>
+
+            <td>{candidat.codeacademyprogress}</td>
+
+            <td>{candidat.assistedtoinformativesession ? "true" : "false"}</td>
+
+            <td>{candidat.bootcamp.bootcampName}</td>
+
+            <td>{candidat.processState.name}</td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 }
