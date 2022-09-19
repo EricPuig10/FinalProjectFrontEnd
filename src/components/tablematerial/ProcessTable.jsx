0c4b4eb126be346edae227ce20e241d2b71495eb
EricPuig10 <puigvendrelloetona@gmail.com>
2022-09-19 15:26:55 +0200

    modal updated

---
+++ src/components/tablematerial/ProcessTable.jsx
@@ -0,0 +1,99 @@
+import * as React from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { useState } from "react";
+import { useEffect } from "react";
+import { CtTabBut, TableButton } from "./DataTable.styled";
+
+import { Link, useLocation } from "react-router-dom";
+import { processService } from "../../services/processService";
+
+export function BootcampTable() {
+  const [process, setProcess] = useState([]);
+
+  useEffect(() => {
+    getAllProcess();
+  }, []);
+
+  const getAllProcess = () => {
+    processService.getAllProcess().then((res) => {
+      setProcess(res);
+    });
+  };
+
+//   const location = useLocation();
+
+  const columns = [
+    {
+      field: "Actions",
+      renderCell: (cellValues) => {
+        return (
+          <>
+            <CtTabBut>
+              <TableButton
+                variant="contained"
+                color="primary"
+                // onClick={() => {
+                //   deleteProcess(cellValues.row.id);
+                // }}
+              >
+                <i className="fa-regular fa-trash-can fa-lg"></i>
+              </TableButton>
+
+              <TableButton variant="contained" color="primary">
+                <Link to={`/bootcamps/${cellValues.row.id}`}>
+                  <i className="fa-regular fa-file fa-lg"></i>
+                </Link>
+              </TableButton>
+            </CtTabBut>
+          </>
+        );
+      },
+    },
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "name",
+      headerName: "Nombre",
+      width: 130,
+      renderCell: (params) => {
+        return (
+          <Link to={`/process/${params.row.id}/candidats`}>
+            <div className="rowitem">{params.row.name}</div>
+          </Link>
+        );
+      },
+    },
+    
+  ];
+
+  return (
+    <>
+      <div
+        style={{
+          height: 420,
+          width: "90%",
+          paddingRight: "4%",
+          marginLeft: "10%",
+          marginTop: "2.5%",
+          backgroundColor: "#fbfbfb",
+          headerName: "#9d4848",
+        }}
+      >
+        <DataGrid
+          columns={columns}
+          rows={process}
+          pageSize={10}
+          rowsPerPageOptions={[10]}
+        />
+      </div>
+      {/* {location.pathname === "/bootcamps/create" ? null : (
+        <Link to="/bootcamps/create">
+          <BtnAdd>
+            <i className="fa-solid fa-plus fa-2xl"></i>
+          </BtnAdd>
+        </Link>
+      )} */}
+    </>
+  );
+}
+
+export default BootcampTable;
---
+++ src/pages/ProcessPage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Navbar } from "../components/navbar/Navbar";
+import { Sidebar } from "../components/sidebar/Sidebar";
+import ProcessTable from "../components/tablematerial/ProcessTable";
+
+export const ProcessPage = () => {
+  return (
+    <>
+      <Navbar />
+      <div>
+        <Sidebar />
+      </div>
+        <ProcessTable />
+    </>
+  );
+};
+
+export default ProcessPage;
