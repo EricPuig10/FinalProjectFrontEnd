133f8cf27d3d9ff5fef21adcf7ba924a0ba7b792
EricPuig10 <puigvendrelloetona@gmail.com>
2022-09-22 13:04:26 +0200

    merged dev with imports solved

--- src/pages/CandidatsByBootcampPage.jsx
+++ src/pages/CandidatsByBootcampPage.jsx
@@ -1,4 +1,4 @@
-import Navbar from "../components/navbar/Navbar";
+import {Navbar} from "../components/navbar/Navbar";
 import { Sidebar } from "../components/sidebar/Sidebar";
 import { CandidatsByBootcampTable } from "../components/tablematerial/CandidatsByBootcampTable";
 
